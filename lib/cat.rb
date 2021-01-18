320576c06d944d84dbd411a09b0961f4e5092607
HannaBM12 <Hannabmulugeta@gmail.com>
2021-01-18 08:02:46 -0500

    commit tree 1e5c56f136b23f5a7f7365849895b2bfd8b848f8

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Cat
   # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+    def initialize(name, owner, mood = "nervous")
+      @name = name
+      @owner = owner
+      @mood = mood
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.count
+      self.all.length
+    end
+
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Owner
   # code goes here
+  @@all = []
+  attr_reader :name, :species
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  def self.count
+    self.all.length
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
 end
